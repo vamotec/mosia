b938ddd6d7629af051a75ad65014e4d848cb8ae8
vamotec <vamotec@icloud.com>
2025-09-02 23:07:55 +0800

    fix: ProtobufCodec缺少了依赖

--- backend/main_service/moscala/src/main/scala/app/mosia/core/redis/ProtobufCodecSupplier.scala
+++ backend/main_service/moscala/src/main/scala/app/mosia/core/redis/ProtobufCodecSupplier.scala
@@ -1,7 +1,8 @@
 package app.mosia.core.redis
 
-import zio.{redis, schema}
-import zio.schema.codec
+import zio.redis.*
+import zio.schema.*
+import zio.schema.codec.*
 
 object ProtobufCodecSupplier extends CodecSupplier {
   override def get[A: Schema]: BinaryCodec[A] = ProtobufCodec.protobufCodec
